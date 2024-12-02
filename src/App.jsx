af9ee4bd7f951b1cd2f6771ba373de061206f56a
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-12-02 14:28:10 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './components/LogIn/Login'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from "./feature/authSlice"
 
 function App() {
-  return (
-    <>
-    </>
-  )
+  const [loading, setLoading] = new useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+      // .catch()
+    }).finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+  <div className='min-h-screen'>test</div>
+  ) : null //improve here
 }
 
 export default App
